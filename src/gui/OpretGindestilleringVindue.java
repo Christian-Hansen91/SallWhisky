2f34c6bb2bba83c7102065de536931f4e8df1568
alice <eaa23aml@students.eaaa.dk>
2024-05-22 15:07:27 +0200

    Alice blaa onsdag

--- src/gui/OpretGindestilleringVindue.java
+++ src/gui/OpretGindestilleringVindue.java
@@ -3,7 +3,6 @@ package gui;
 import controller.Controller;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -15,20 +14,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class OpretGindestilleringVindue extends Stage implements LagerenhedsVindue {
-    private Label lblDatoer = new Label("Vælg start- og slutdato:");
     private DatePicker dpStartdato = new DatePicker();
     private DatePicker dpSlutdato = new DatePicker(LocalDate.now());
-    private Label lblNavn = new Label("Giv din gin et navn:");
     private TextField txfNavn = new TextField();
-    private Label lblVandTilfoejet = new Label("Vand tilføjet: ");
     private TextField txfVandtilfoejet = new TextField();
-    private Label lblAlkoholprocent = new Label("Alkoholprocent: ");
     private TextField txfAlkoholprocent = new TextField();
-    private Label lblLiter = new Label("Mængde (L): ");
     private TextField txfLiter = new TextField();
-    private Label lblMaengdeEnebaer = new Label("Enebær (g): ");
     private TextField txfEnebaer = new TextField();
-    private Label lblKommentar = new Label("Kommentar: ");
     private TextArea txaKommentar = new TextArea();
     private Label lblOverskrift = new Label("Opret Gindestillering");
     private Button btnGem = new Button("Gem");
@@ -36,14 +28,12 @@ public class OpretGindestilleringVindue extends Stage implements LagerenhedsVind
     private Gindestillering gindestillering = null;
     private Medarbejder medarbejder;
     private StartVindue startVindue;
-    private Button btnVaelgLager = new Button("Lagerplads");
+    private Button btnLagerplads = new Button("Lagerplads");
     private Lager lager = null;
     private int reol, hylde;
-    private Label lblLager = new Label("");
+    private Label lblValgtLager = new Label("");
     private Label lblTilfoejIngrediens = new Label("Tilføj ingredienser:");
-    private Label lblIngrediensMaengde = new Label("Mængde: ");
     private TextField txfIngrediensMaengde = new TextField();
-    private ArrayList<Ingrediensmaengde> ingredienser = new ArrayList<>();
     private ListView<Ingrediensmaengde> lvIngrediensmaengder;
     private ComboBox<Ingrediens> cbIngredienser;
     private Button btnTilfoejIngrediens, btnOpret;
@@ -71,113 +61,101 @@ public class OpretGindestilleringVindue extends Stage implements LagerenhedsVind
         pane.setVgap(10);
         pane.setStyle("-fx-background-image: url('https://migogaarhus.dk/wp-content/uploads/2021/01/Sall-whisky.jpg')");
 
-        lblVandTilfoejet.setTextFill(Color.BURLYWOOD);
-        lblAlkoholprocent.setTextFill(Color.BURLYWOOD);
-        lblKommentar.setTextFill(Color.BURLYWOOD);
         lblOverskrift.setTextFill(Color.BURLYWOOD);
-        lblLiter.setTextFill(Color.BURLYWOOD);
-        lblDatoer.setTextFill(Color.BURLYWOOD);
-        lblLager.setTextFill(Color.BURLYWOOD);
-        lblMaengdeEnebaer.setTextFill(Color.BURLYWOOD);
         lblTilfoejIngrediens.setTextFill(Color.BURLYWOOD);
-        lblIngrediensMaengde.setTextFill(Color.BURLYWOOD);
-        lblNavn.setTextFill(Color.BURLYWOOD);
+        lblValgtLager.setTextFill(Color.BURLYWOOD);
 
-        pane.add(lblOverskrift, 0, 0, 9, 1);
+        pane.add(lblOverskrift, 0, 0, 6, 1);
         pane.setHalignment(lblOverskrift, HPos.CENTER);
 
-        //pane.add(lblDatoer, 0,1,2,1);
         pane.add(dpStartdato, 0, 2, 2, 1);
         dpStartdato.setPromptText("Startdato");
+        dpStartdato.setPrefWidth(175);
+        dpSlutdato.setPrefWidth(175);
         pane.add(dpSlutdato, 0, 3, 2, 1);
         dpSlutdato.setPromptText("Slutdato");
 
         dpStartdato.getEditor().setDisable(true);
         dpSlutdato.getEditor().setDisable(true);
 
-        //pane.add(lblNavn, 0, 4, 2, 1);
         pane.add(txfNavn, 0, 1, 2, 1);
         txfNavn.setPromptText("Navngiv gin:");
         pane.setHalignment(txfNavn, HPos.RIGHT);
 
         pane.add(txaKommentar, 0, 4, 2, 2);
         txaKommentar.setPromptText("Tilføj kommentar:");
-        txaKommentar.setMaxWidth(175);
+        txaKommentar.setPrefWidth(175);
+        txaKommentar.setMaxHeight(60);
         pane.setHalignment(txaKommentar, HPos.LEFT);
 
-        //pane.add(lblAlkoholprocent, 0, 6);
         pane.add(txfAlkoholprocent, 0, 6);
-        txfAlkoholprocent.setMaxWidth(75);
+        txfAlkoholprocent.setMaxWidth(85);
         txfAlkoholprocent.setPromptText("Alkoholprocent:");
         pane.setHalignment(txfAlkoholprocent, HPos.RIGHT);
         pane.add(txfVandtilfoejet, 1, 6);
         txfVandtilfoejet.setPromptText("Vand tilføjet:");
-        txfVandtilfoejet.setMaxWidth(75);
+        txfVandtilfoejet.setMaxWidth(85);
         pane.setHalignment(txfVandtilfoejet, HPos.RIGHT);
 
-
-        //pane.add(lblMaengdeEnebaer, 2, 1);
-        pane.add(txfEnebaer, 2, 1);
+        pane.add(txfEnebaer, 2, 1, 2, 1);
         txfEnebaer.setPromptText("Enebær (g):");
         pane.setHalignment(txfEnebaer, HPos.RIGHT);
         btnOpret = new Button("Opret");
         pane.add(btnOpret, 3, 2);
+        btnOpret.setPrefWidth(85);
+        pane.setHalignment(btnOpret, HPos.RIGHT);
         btnOpret.setOnAction(e -> opretGindestillering());
         pane.add(txfLiter, 2, 2);
         txfLiter.setPromptText("Liter i alt:");
         pane.setHalignment(txfLiter, HPos.LEFT);
-        txfLiter.setMaxWidth(75);
+        txfLiter.setPrefWidth(85);
 
         pane.add(lblTilfoejIngrediens, 2, 3, 2, 1);
         pane.setHalignment(lblTilfoejIngrediens, HPos.CENTER);
         cbIngredienser = new ComboBox<>();
         cbIngredienser.setPromptText("Vælg ingrediens");
         pane.setHalignment(cbIngredienser, HPos.RIGHT);
-        cbIngredienser.setPrefWidth(225);
+        cbIngredienser.setPrefWidth(175);
         pane.add(cbIngredienser, 2, 4, 2, 1);
         cbIngredienser.getItems().setAll(Ingrediens.values());
         cbIngredienser.setDisable(true);
 
-        //Tilføj combobox eller lignende med enum ingredienser, måske man kan skrive hvad
-        // som helst og så oprettes ny enum automatisk?? Sejt
-        //man kan ikke lave enum på runtime :(
-
-        //pane.add(lblIngrediensMaengde, 2, 5);
         pane.add(txfIngrediensMaengde, 2, 5, 2, 1);
         txfIngrediensMaengde.setPromptText("Mængde (g):");
         txfIngrediensMaengde.setDisable(true);
+        txfIngrediensMaengde.setPrefWidth(175);
         pane.setHalignment(txfIngrediensMaengde, HPos.RIGHT);
 
         btnTilfoejIngrediens = new Button("Tilføj ingrediens");
         pane.add(btnTilfoejIngrediens, 2, 6, 2, 1);
+        btnTilfoejIngrediens.setPrefWidth(175);
         pane.setHalignment(btnTilfoejIngrediens, HPos.RIGHT);
+
         btnTilfoejIngrediens.setOnAction(e -> tilfoejIngrediens());
         btnTilfoejIngrediens.setDisable(true);
 
-
-        //pane.add(lblVandTilfoejet, 2, 6);
-        //pane.add(lblLiter, 4,1);
-
-
-        //pane.add(lblKommentar, 4, 2, 2, 1);
-
         lvIngrediensmaengder = new ListView<>();
-        pane.add(lvIngrediensmaengder, 5, 1, 3, 4);
+        pane.add(lvIngrediensmaengder, 4, 1, 2, 4);
+        lvIngrediensmaengder.setMinWidth(175);
 
+        pane.add(btnLagerplads, 4, 5);
+        btnLagerplads.setPrefWidth(85);
+        pane.setHalignment(btnLagerplads, HPos.LEFT);
+        btnLagerplads.setDisable(true);
+        pane.add(lblValgtLager, 5, 5);
+        lblValgtLager.setMaxWidth(85);
+        pane.setHalignment(lblValgtLager, HPos.RIGHT);
 
-        pane.add(btnVaelgLager, 5, 5);
-        pane.setHalignment(btnVaelgLager, HPos.LEFT);
-        btnVaelgLager.setDisable(true);
-        pane.add(lblLager, 6, 5, 2, 1);
-
-        pane.add(btnGem, 7, 6);
-        pane.add(btnAnnuller, 5, 6);
+        pane.add(btnGem, 5, 6);
+        pane.add(btnAnnuller, 4, 6);
         pane.setHalignment(btnGem, HPos.RIGHT);
+        btnGem.setPrefWidth(85);
+        btnAnnuller.setPrefWidth(85);
         pane.setHalignment(btnAnnuller, HPos.LEFT);
         btnGem.setDisable(true);
         btnGem.setOnAction(event -> gemAction());
         btnAnnuller.setOnAction(event -> annullerAction());
-        btnVaelgLager.setOnAction(event -> vaelgLagerAction());
+        btnLagerplads.setOnAction(event -> vaelgLagerAction());
     }
 
     private void opdaterIngrediensLv() {
@@ -193,7 +171,7 @@ public class OpretGindestilleringVindue extends Stage implements LagerenhedsVind
 
     private void opdaterLagerLabels() {
         if (lager != null) {
-            lblLager.setText("Lager: " + lager.getNavn() + ", reol " + reol + " hylde " + hylde);
+            lblValgtLager.setText("Lager: " + lager.getNavn() + ", reol " + reol + " hylde " + hylde);
         }
     }
 
@@ -272,7 +250,7 @@ public class OpretGindestilleringVindue extends Stage implements LagerenhedsVind
 
         cbIngredienser.setDisable(false);
         txfIngrediensMaengde.setDisable(false);
-        btnVaelgLager.setDisable(false);
+        btnLagerplads.setDisable(false);
         btnTilfoejIngrediens.setDisable(false);
         btnGem.setDisable(false);
     }
