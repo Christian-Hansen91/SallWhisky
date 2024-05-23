1990e0b15efce2e695465a7db7d82d65b44f06ea
Simon Høgh <simonahlehoegh@gmail.com>
2024-05-23 08:33:23 +0200

    Bug fix

--- src/model/application/Gindestillering.java
+++ src/model/application/Gindestillering.java
@@ -76,7 +76,7 @@ public class Gindestillering implements Lagerenhed {
     public void tilfoejLager(Lager lager, int reol, int hylde) {
         if (!lager.equals(this.lager)) {
             this.lager = lager;
-            lager.addLagerenhedAt(reol, hylde, this);
+            //lager.addLagerenhedAt(reol, hylde, this);
         }
     }
 
